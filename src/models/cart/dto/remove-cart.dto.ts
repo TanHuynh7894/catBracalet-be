c371e9cdcab22cf308ea06990963563d4d4b4c1c
ThuHienHenith <hiendttse184573@fpt.edu.vn>
2026-05-20 13:29:49 +0700

    delete redundant folders

--- src/models/cart/dto/remove-cart.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-
-export class RemoveCartDto {
-    @ApiProperty({
-    example: '123e4567-e89b-12d3-a456-426614174000',
-    description: 'The ID of the cart to remove'})
-    cartId: string;
-}
