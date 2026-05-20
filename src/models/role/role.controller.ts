6712661d5d1ee6230c6a8d261096f36752e3e93b
TruongNguyen <xuantruong124.work@gmail.com>
2026-05-21 00:10:18 +0700

    update note for api

--- src/models/role/role.controller.ts
+++ src/models/role/role.controller.ts
@@ -1,6 +1,6 @@
 
 import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
@@ -9,30 +9,35 @@ import { RemoveRoleDto } from './dto/remove-role.dto';
 @ApiTags('Role')
 @Controller('role')
 export class RoleController {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(private readonly roleService: RoleService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new role' })
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all roles' })
   findAll() {
     console.log('✅ Received GET request for /role');
     return this.roleService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get role details by ID' })
   findOne(@Param('id') id: string) {
     return this.roleService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update role information' })
   update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
     return this.roleService.update(id, updateRoleDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove or update role status' })
   remove(@Param('id') id: string, @Query() removeRoleDto: RemoveRoleDto) {
     return this.roleService.remove(id, removeRoleDto.status);
   }
